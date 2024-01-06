22e6ac83dde7cf2f790d8d92c15166d00f0a3151
unknown <usern5949@gmail.com>
2024-01-06 22:30:13 +0530

    Added sending HTML pages back to client

--- node-crash-course/server.js
+++ node-crash-course/server.js
@@ -1,9 +1,31 @@
 const http = require("http")
-
+const fs = require("fs")
 const server = http.createServer((req, res)=>{
-    console.log("Request Made")
-
+    // console.log(req.url)
+    // console.log(req.method)
+    // console.log("Request Made")
+    //set header content type
+    // res.setHeader("Content-Type", "text/plain")
+    // res.write("Hello Ninjas")
+    // res.end()
+    // res.setHeader("Content-Type", "text/html")
+    // res.write("<head><link rel='stylsheet' href='#'></head>")
+    // res.write("<p>Hello Ninjas</p>")
+    // res.write("<p>Hello again, Ninjas</p>")
+    
+    //send an HTML file
+    fs.readFile("./views/index.html", (err, data)=>{
+        if(err){
+            console.log(err)
+            res.end()
+        }
+        else{
+            // res.write(data)
+            // res.end()
+            res.end(data)
+        }
+    })
 })
 server.listen(3000, "localhost", ()=>{
-    console.log("Listeninhg for requests on port 3000")
+    console.log("Listening for requests on port 3000")
 })
