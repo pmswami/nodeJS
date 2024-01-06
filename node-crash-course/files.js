518e12635a527b8b6ddade11c5984c40a78159d4
unknown <usern5949@gmail.com>
2024-01-06 18:36:29 +0530

    Added workings with the file

--- node-crash-course/files.js
+++ node-crash-course/files.js
@@ -6,17 +6,44 @@ const fs = require("fs")
 // })
 // console.log("Last line")
 
-//writing modules
-fs.writeFile("./docs/blog1.txt", "Hello World", (err)=>{
-    console.log("File written successfully")
-})
+// //writing modules
+// fs.writeFile("./docs/blog1.txt", "Hello World", (err)=>{
+//     console.log("File written successfully")
+// })
 
-fs.writeFile("./docs/blog2.txt", "Hello World", (err)=>{
-    console.log("File written successfully")
-})// creates a new file if it doesnt exists
+// fs.writeFile("./docs/blog2.txt", "Hello World", (err)=>{
+//     console.log("File written successfully")
+// })// creates a new file if it doesnt exists
 
 // directories
-
-
+if(!fs.existsSync("./assets")) {
+    fs.mkdir("./assets", (err)=>{
+        if(err){
+            console.log(err)
+        }
+        console.log("Folder Created")
+    })
+}
+else{
+    console.log("directory already esists")
+    fs.rmdir("./assets", (err)=>{
+        console.log(err)
+    })
+    console.log("Folder Deleted")
+}
 
 // deleting files
+if(fs.existsSync("./docs/deleteme.txt")){
+    fs.unlink("./docs/deleteme.txt", (err)=>{
+        if(err){
+            console.log(err)
+        }
+        console.log("File Deleted")
+    })
+}
+else{
+    fs.writeFile("./docs/deleteme.txt", "", (err)=>{
+        console.log(err)
+    })
+    console.log("File Created")
+}
