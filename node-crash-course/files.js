4681b7322e07084c45b1e71f04523c5a2b27d06e
unknown <usern5949@gmail.com>
2024-01-06 18:25:54 +0530

    Added writing into file

--- node-crash-course/files.js
+++ node-crash-course/files.js
@@ -1,14 +1,19 @@
 const fs = require("fs")
-//reading files
-fs.readFile("./docs/blog1.txt", (err, data)=>{
-    if(err) console.log(err)
-    console.log(data.toString())
-})
-console.log("Last line")
-
+// //reading files
+// fs.readFile("./docs/blog1.txt", (err, data)=>{
+//     if(err) console.log(err)
+//     console.log(data.toString())
+// })
+// console.log("Last line")
 
 //writing modules
+fs.writeFile("./docs/blog1.txt", "Hello World", (err)=>{
+    console.log("File written successfully")
+})
 
+fs.writeFile("./docs/blog2.txt", "Hello World", (err)=>{
+    console.log("File written successfully")
+})// creates a new file if it doesnt exists
 
 // directories
 
