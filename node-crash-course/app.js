7c3e5eb8e83d96bad81f3ef6ba8d892a40576f1a
unknown <usern5949@gmail.com>
2024-01-07 20:16:44 +0530

    Added EJS

--- node-crash-course/app.js
+++ node-crash-course/app.js
@@ -14,13 +14,18 @@ app.get("/", (req, res)=>{
     // console.log("express app")
     // res.send("<p>HomePage</p>")
     // res.sendFile("./views/index.html",{root:__dirname})
-    res.render("index")
+    const blogs = [
+        {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+        {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+        {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+      ];
+    res.render("index", {title: "Home", blogs:blogs})
 }) 
 
 app.get("/about", (req, res)=>{
     // res.send("<p>About</p>")
     // res.sendFile("./views/about.html",{root:__dirname})
-    res.render("about")
+    res.render("about",{title: "About"})
 })
 
 // //app redirect
@@ -29,13 +34,13 @@ app.get("/about", (req, res)=>{
 // })
 
 app.get("/blogs/create", (req, res)=>{
-    res.render("create")
+    res.render("create",{title: "Create New Blog"})
 })
 
 //404 page
 app.use((req, res)=>{
     // res.status(404).sendFile("./views/404.html",{root:__dirname})
-    res.status(404).render("404")
+    res.status(404).render("404", {title: "404"})
 })//use this function for any other request coming in, always should be at bottom of all request type
 
 
